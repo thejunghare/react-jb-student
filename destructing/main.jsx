198f8aaca2a40aeede4856d1718b23a70d56e8fa
thejunghare <prasadjunghare2@gmail.com>
2026-04-21 13:37:21 +0530

    updated repo

--- destructing/main.jsx
+++ destructing/main.jsx
@@ -10,38 +10,28 @@ root.render(
     Contact()
 )
 
-function Contact({ img, name, email, phone }) {
+function Contact({ props }) {
     return (
         <article className="contact-card">
             <img
-                src={img}
+                src={props.img}
                 alt="Photo of Mr. Whiskerson"
             />
-            <h3>{name}</h3>
+            <h3>{props.name}</h3>
             <div className="info-group">
                 <img
                     src="./images/phone-icon.png"
                     alt="phone icon"
                 />
-                <p>{phone}</p>
+                <p>{props.phone}</p>
             </div>
             <div className="info-group">
                 <img
                     src="./images/mail-icon.png"
                     alt="mail icon"
                 />
-                <p>{email}</p>
+                <p>{props.email}</p>
             </div>
         </article>
     )
 }
-/*
-const person = {
-    img: "./images/mr-whiskerson.png",
-    name: "Mr. Whiskerson",
-    phone: "(800) 555-1234",
-    email: "mr.whiskaz@catnap.meow"
-}
-person.img -> img
-const {img, name} = person
-*/
--- js-revision/map-method.js
+++ js-revision/map-method.js
@@ -5,7 +5,7 @@ Given an array of numbers, return an array of each number, squared
 const nums = [1, 2, 3, 4, 5]
 // -->       [1, 4, 9, 16, 25]
 // Your code here
-let result = nums.map(element => element * element)
+
 
 
 /*
@@ -17,9 +17,6 @@ the first letter of each string is capitalized
 const names = ["alice", "bob", "charlie", "danielle"]
 // -->        ["Alice", "Bob", "Charlie", "Danielle"]
 // Your code here
-names.map(n => (
-    n.charAt(0).toUpperCase() + n.slice(1)
-))
 
 
 
